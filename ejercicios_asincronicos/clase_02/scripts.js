cb89ad3d37e29453d622b159778a491a32e301c7
IsabelAntolin <isaabuu@gmail.com>
2022-06-13 12:21:55 -0400

    cambio

--- ejercicios_asincronicos/clase_02/scripts.js
+++ ejercicios_asincronicos/clase_02/scripts.js
@@ -1,6 +1,6 @@
 function mostrarMensaje() {
-   //  const fecha = new Date() //Devuelve dia u hora
-   let diaSemana = (new Date()).getDay();
+   const fecha = new Date() //Devuelve dia u hora
+   let diaSemana = fecha.getDay();
    let listaSemana = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'sábado', 'domingo'];
    mensaje = alert('Hoy es ' + listaSemana[diaSemana]);
    return mensaje;
